cc9ce3fc469976536529b7c9c23f28b79e58c272
Afsalms <afsalms93.mec@gmail.com>
2018-12-11 20:32:00 +0530

    Recycler view

--- app/src/main/java/com/example/qbuser/recyclerviewtutorial/adaptor/CustomAdapter.kt
+++ app/src/main/java/com/example/qbuser/recyclerviewtutorial/adaptor/CustomAdapter.kt
@@ -7,21 +7,20 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.example.qbuser.recyclerviewtutorial.R
 import com.example.qbuser.recyclerviewtutorial.models.User
-import kotlinx.android.synthetic.main.list_items.view.*
 
 
 class CustomAdapter(val userList: ArrayList<User>): RecyclerView.Adapter<CustomAdapter.ViewHolder>(){
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        holder?.txtName?.text = userList[position].name
-        holder?.txtTitle.text = userList[position].title
+        holder.txtName?.text = userList[position].name
+        holder.txtTitle.text = userList[position].title
 
     }
 
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent?.context).inflate(R.layout.list_items,parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_items,parent, false)
         return ViewHolder(v)
 
     }
